6493ca1c7735828f53d5d2f4b9a97c43c87bad9c
eujjong18 <eujjong18@naver.com>
2023-12-06 23:35:14 +0900

    feat #3: RefreshToken 엔티티 설계

---
+++ src/main/java/com/study/myboard/domain/user/model/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.study.myboard.domain.user.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_token_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+}
